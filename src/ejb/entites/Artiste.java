64f600776622c0d7427af39c175230548ea0920b
Hamza El-kharroubi <hamza.elkharrouri.etu@univ-lille.fr>
2021-02-24 09:09:04 +0100

    Création de la classe Article

---
+++ src/ejb/entites/Artiste.java
@@ -0,0 +1,20 @@
+package ejb.entites;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Article implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue
+	public int getNum(){
+		return this.Num;
+	}
+	
+	@Column private String Nom;
+	@Column private int Prenom;
+}
